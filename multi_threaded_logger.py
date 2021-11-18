ab17629bb7fcd783f02caf2e0742577e4d8b553c
oliver <498946975@qq.com>
2021-11-18 10:19:50 +0800

    handlers

--- multi_threaded_logger.py
+++ multi_threaded_logger.py
@@ -4,23 +4,25 @@ import logging
 from setup_log import setup_logging
 
 
-def test_func():
-    logging.info("start func")
-    logging.info("exec func")
-    logging.info("end func")
+def test_func(num):
+    logging.info("start func" + str(num))
+    logging.info("exec func" + str(num))
+    logging.info("end func" + str(num))
+    logging.error("test error log" + str(num))
+
 
 
 # 发送debug级别日志消息
-def test():
-    time.sleep(3)  # 每个线程睡3秒
+def test(num):
+    time.sleep(2)  # 每个线程睡2秒
     setup_logging("./logging.yaml")
-    test_func()
+    test_func(num)
 
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=2)  # 一次起2个线程
     for i in range(10):  # 循环10次
-        pool.apply_async(func=test)
+        pool.apply_async(func=test, args=(i,))
     pool.close()
     pool.join()
     print('done')
--- setup_log.py
+++ setup_log.py
@@ -4,13 +4,14 @@ import os
 
 
 def setup_logging(default_path="./logging.yaml", default_level=logging.INFO):
-    path = default_path
-    if os.path.exists(path):
-        with open(path, "r") as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-            logging.config.dictConfig(config)
-    else:
-        logging.basicConfig(level=default_level)
-
-
+    try:
+        path = default_path
+        if os.path.exists(path):
+            with open(path, "r") as f:
+                config = yaml.load(f, Loader=yaml.FullLoader)
+                logging.config.dictConfig(config)
+        else:
+            logging.basicConfig(level=default_level)
+    except Exception as e:
+        print(e)
 
